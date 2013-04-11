6c39dbb14fbda1d7d667279191aa7c072dbaefb6
Fuzzzie <fuzzzzie@gmail.com>
2013-04-11 01:25:58 -0400

    cleanup

--- common/FuzzCraft/Helpers/BlockHelper.java
+++ common/FuzzCraft/Helpers/BlockHelper.java
@@ -1,8 +1,6 @@
 package FuzzCraft.Helpers;
 
-import net.minecraftforge.common.MinecraftForge;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+
 
 public class BlockHelper {
     
